f8db4ecb699a57b1471b29e46a68532d8cee7111
Gabriela Machalska <73642383+gabrielamachalska@users.noreply.github.com>
2021-01-25 19:17:43 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -7,3 +7,19 @@
 #include <string>
 
 // tutaj klasa Warzywo
+class Warzywo
+{ 
+    std::string nazwa;
+    double      cena;
+    int         kolor;
+    static unsigned liczba_warzyw();
+
+public:
+    Warzywo(const std::string& n, double c, int k) : nazwa(n), cena(c), kolor(k) { ++liczba_warzyw; }
+    ~Warzywo() { --liczba_warzyw; };
+    static unsigned getVeg() { return liczba_warzyw; }
+    void            opis(std::ostream& cout) const
+    {
+        cout << "[" << nazwa << "]: [ " << cena << "], [" << kolor << "]" std::endl;
+    }
+};
