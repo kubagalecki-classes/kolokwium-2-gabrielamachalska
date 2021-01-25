dc5abcac7746cf4cd85e8dbb7bed717d3acb6e41
Gabriela Machalska <73642383+gabrielamachalska@users.noreply.github.com>
2021-01-25 19:18:15 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -4,3 +4,20 @@
 #include "chrup.hpp"
 
 // tutaj klasy Ogorek, Zielony i Kiszony
+class Ogorek
+{
+public: 
+	virtual std::string chrup() = 0;
+
+};
+
+class Zielony : public Ogorek
+{
+    std::string chrup() {return chrupZielony}
+};
+
+class Kiszony : public Ogorek
+{
+    std::string chrup() {return chrupKiszony}
+};
+
